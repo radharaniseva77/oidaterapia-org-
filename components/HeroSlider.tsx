58ce8cdd0ddff49ab0e5c7883d514e65efe25a36
appecodestinos <colectandoparatemple@gmail.com>
2026-04-15 06:49:19 -0500

    fix: forzar refresco de imagen del banner

--- components/HeroSlider.tsx
+++ components/HeroSlider.tsx
@@ -6,7 +6,7 @@ import { motion, AnimatePresence } from "framer-motion";
 const images = [
   "/assets/oidaterapia/slider-1.jpg",
   "/assets/oidaterapia/slider-2.jpg",
-  "/assets/oidaterapia/slider-03.jpg?v=1",
+  "/assets/oidaterapia/slider-3.jpg?v=1",
   "/assets/oidaterapia/slider-4.jpg",
   "/assets/oidaterapia/slider-5.jpg"
 ];
